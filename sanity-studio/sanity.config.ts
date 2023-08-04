5d134e38c001e0c1f689bc9258ed185530a5ae6e
Jo Carr <hulaus21@gmail.com>
2023-08-03 17:04:48 -0700

    added schema types

--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -4,15 +4,15 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
-  name: 'default',
-  title: 'Sanity Next.js Site',
-
-  projectId: '0ow48ytt',
-  dataset: 'production',
+  name: 'sanity-nextjs-site', //Used to differentiate Workspaces. Not Compulsory for single workspace setup
+  title: 'Sanity Next.js Site', // Title of Project. Show up in the Studio
+  projectId: '0ow48ytt', // Unique ID that points to the Sanity project you're working with
+  dataset: 'production', // The name of the dataset to use for your studio
+  basePath: "/studio", // The URL Path where the studio will be mounted
 
   plugins: [deskTool(), visionTool()],
 
-  schema: {
-    types: schemaTypes,
+  schema: { //The object where the schema files will be defined
+    types: schemaTypes, 
   },
 })
